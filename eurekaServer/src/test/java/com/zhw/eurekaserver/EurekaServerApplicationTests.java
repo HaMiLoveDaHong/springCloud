e9fe7245aca581936afadcb58f1f6d372e6cb6cc
zhanghongwei <1501125728@qq.com>
2020-08-31 14:41:58 +0800

    1.commit eurekaserver moudle

---
+++ eurekaServer/src/test/java/com/zhw/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhw.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
